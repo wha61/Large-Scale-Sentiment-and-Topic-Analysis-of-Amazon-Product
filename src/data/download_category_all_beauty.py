da0c539851349cbe512eede71d341de0bca05126
Chenzheng Li <chenzheng_li@sfu.ca>
2025-11-20 18:05:37 -0800

    update downloader

--- src/data/download_category_all_beauty.py
+++ src/data/download_category_all_beauty.py
@@ -11,10 +11,10 @@ def download_category(category="All_Beauty"):
         trust_remote_code=True
     )
 
-    out_path = f"data/raw/{category}_reviews.parquet"
+    out_path = f"data/raw/{category}_reviews"
     print(f"Saving to {out_path}")
     ds.to_parquet(out_path)
     print("Done!")
 
 if __name__ == "__main__":
-    download_category("All_Beauty")
+    download_category("all_beauty")
