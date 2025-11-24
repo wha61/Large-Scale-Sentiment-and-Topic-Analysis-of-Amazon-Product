2d1f3f4555c80f8678a09e917d6563ac9d3e40f3
wha61 <wha61@sfu.ca>
2025-11-23 18:39:44 -0800

    add: specify pyarrow, datasets version in requirments.txt || fix: remove trust_remote_code and add directory creation in download_category_all_beauty.py

--- src/data/download_category_all_beauty.py
+++ src/data/download_category_all_beauty.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+import os
 
 def download_category(category="All_Beauty"):
     config = f"raw_review_{category}"
@@ -8,13 +9,14 @@ def download_category(category="All_Beauty"):
         "McAuley-Lab/Amazon-Reviews-2023",
         config,
         split="full",
-        trust_remote_code=True
+        # trust_remote_code=True
     )
 
     out_path = f"data/raw/{category}_reviews"
+    os.makedirs(out_path, exist_ok=True) 
     print(f"Saving to {out_path}")
     ds.to_parquet(out_path)
     print("Done!")
 
 if __name__ == "__main__":
-    download_category("all_beauty")
+    download_category("All_Beauty")
